cd1bf4f2711a9c75fccb92ead84f5f63f6f1ed1e
Marius <Marius@DESKTOP-9T0OFRM>
2023-01-08 22:25:09 +0100

    commit de ordenado

--- Warehouse1/src/warehouse/LineaFactura.java
+++ Warehouse1/src/warehouse/LineaFactura.java
@@ -5,35 +5,41 @@ public class LineaFactura {
 	private int numero;
 	private Articulo articulo;
 	private int cantidad;
-	
+
 	public LineaFactura() {
-		
+
 	}
-	
+
 	public LineaFactura(int numero, Articulo articulo, int cantidad) {
 		this.numero = numero;
 		this.articulo = articulo;
 		this.cantidad = cantidad;
 	}
+
 	public int getNumero() {
 		return numero;
 	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
 	public Articulo getArticulo() {
 		return articulo;
 	}
+
 	public void setArticulo(Articulo articulo) {
 		this.articulo = articulo;
 	}
+
 	public int getCantidad() {
 		return cantidad;
 	}
+
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
-	
+
 	public double precioTotal() {
 		return this.articulo.getPrecio() * this.cantidad;
 	}
