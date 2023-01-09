bcce3177a02f3da75c2c0904f8d9e5c1f76e01e7
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-09 14:19:11 +0100

    Commit de enlace

---
+++ Warehouse1/src/warehouse/Cerveza.java
@@ -0,0 +1,51 @@
+package warehouse;
+
+public class Cerveza extends Articulo {
+
+	private String origen;
+	private String cereales;
+	private double gradosAlcohol;
+
+    public Cerveza() {
+		
+	}
+	
+	public Cerveza(String origen, String cereales, double gradosAlcohol) {
+		this.origen = origen;
+		this.cereales = cereales;
+		this.gradosAlcohol = gradosAlcohol;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public String getCereales() {
+		return cereales;
+	}
+
+	public void setCereales(String cereales) {
+		this.cereales = cereales;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+	
+	public boolean esFuerte() {
+		if (this.gradosAlcohol > 7) 
+			return true;
+		
+		return false;
+	}
+
+	
+}
---
+++ Warehouse1/src/warehouse/Vino.java
@@ -0,0 +1,71 @@
+package warehouse;
+
+public class Vino extends Articulo {
+
+	private String color;
+	private String origen;
+	private int año;
+	private String tipoUva;
+	private double gradosAlcohol;
+	
+	public Vino() {
+		
+	}
+
+	public Vino(String color, String origen, int anio, String tipoUva, double gradosAlcohol) {
+
+		this.color = color;
+		this.origen = origen;
+		this.año = anio;
+		this.tipoUva = tipoUva;
+		this.gradosAlcohol = gradosAlcohol;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAnio(int anio) {
+		this.año = anio;
+	}
+
+	public String getTipoUva() {
+		return tipoUva;
+	}
+
+	public void setTipoUva(String tipoUva) {
+		this.tipoUva = tipoUva;
+	}
+
+	public double getGradosAlcohol() {
+		return gradosAlcohol;
+	}
+
+	public void setGradosAlcohol(double gradosAlcohol) {
+		this.gradosAlcohol = gradosAlcohol;
+	}
+
+	public boolean esFuerte() {
+		if (this.gradosAlcohol > 13.5) 
+			return true;
+		
+		return false;
+	}
+
+}
