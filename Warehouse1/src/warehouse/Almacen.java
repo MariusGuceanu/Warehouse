e6dafebcff27523fb12e8dcd00561ce4bba8b776
Marius Guceanu <Marius Guceanu@HZ114828.H012108.NET>
2023-01-10 09:40:28 +0100

    commit pls

--- Warehouse1/src/warehouse/Almacen.java
+++ Warehouse1/src/warehouse/Almacen.java
@@ -1,8 +1,27 @@
 package warehouse;
 
+import java.util.ArrayList;
+
 public class Almacen {
+
+	private ArrayList <Articulo> articulos = new ArrayList();
+	
+	
+	
+	public ArrayList<Articulo> getArticulos() {
+		return articulos;
+	}
+
+
+
+	public void setArticulos(ArrayList<Articulo> articulos) {
+		this.articulos = articulos;
+	}
+
+
 
 	public void cargarDatos() {
 
+		
 	}
 }
